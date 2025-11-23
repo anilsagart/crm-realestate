4004a5af25bef1200d1c64b0b9ffd7852ef10fcf
Anilsagar T <aniltsagar@gmail.com>
2025-11-23 23:38:49 +0530

    pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,8 @@ import { AppRoutes } from './routes';
 
 function App() {
   return (
-      <Router>
-      <div className="min-h-screen bg-gray-50">
-        <main>
-          <AppRoutes />
-        </main>
-      </div>
+    <Router>
+      <AppRoutes />
     </Router>
   );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
+import { Login } from './pages/Login/Login';
+import { CRM } from './pages/CRM/CRM';
+import { Dashboard } from './pages/CRM/Dashboard/Dashboard';
+import { Leads } from './pages/CRM/Leads/Leads';
+import { Properties } from './pages/CRM/Properties/Properties';
 
 export const AppRoutes: React.FC = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      {/* <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="*" element={<NotFound />} /> */}
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/crm" element={<CRM />}>
+                <Route path="" element={<Dashboard />} /> 
+                <Route path="dashboard" element={<Dashboard />} /> 
+                <Route path="leads" element={<Leads />} /> 
+                <Route path="properties" element={<Properties />} /> 
+            </Route>
+        </Routes>
+    );
 };
